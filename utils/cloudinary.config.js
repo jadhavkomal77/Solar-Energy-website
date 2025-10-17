9e4956923badebdf3b34ed55d98e7818e31b9998
komal8683 <komal.j8683@gmail.com>
2025-10-17 16:20:09 +0530

    ui added

--- utils/cloudinary.config.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2
-
-cloudinary.config({
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    cloud_name: process.env.CLOUD_NAME,
-})
-
-module.exports = cloudinary
