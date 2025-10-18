e2ec655a09f98b6ceb777e61e83777dd9b45db0a
komal8683 <komal.j8683@gmail.com>
2025-10-18 20:14:11 +0530

    dist added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieparser())
 app.use(express.static(path.join(__dirname, "dist")))
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cors({
     origin:
         process.env.NODE_ENV === 'development' ?
